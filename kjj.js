ce438eae0c09249d555f5d708f55f9801a6b559c
zlh <2465898626@qq.com>
2021-07-29 22:50:36 +0800

    22.50

--- kjj.js
+++ kjj.js
@@ -1,32 +1,33 @@
-let sss=0;
-const kx={
-    core(ke,fun){
-        $(document).keydown(event=>{
-            if(event.ctrlKey&&event.keyCode==ke){
+console.log("1");
+let sss = 0;
+const kx = {
+    core(ke, fun) {
+        $(document).keydown(event => {
+            if (event.ctrlKey && event.keyCode == ke) {
                 fun();
             }
         })
     }
 }
-const keylist=[
-    ()=>{
+const keylist = [
+    () => {
         console.log("弹幕切换")
-        $("[aria-label=弹幕]").click()
-    },()=>{
-        let ad =$(".bui-select-list:first .bui-select-item-active");
-        (ad.next().length?ad.next():ad.siblings().first()).click();
-    },()=>{
-        let ad= $(".bilibili-player-video-btn-speed-menu-wrap .bilibili-player-active");
-        (ad.prev().length?ad.prev():ad.siblings().last()).click();
-    },()=>{
+        $(".bui-switch-input").click()
+    }, () => {
+        let ad = $(".bui-select-list:first .bui-select-item-active");
+        (ad.next().length ? ad.next() : ad.siblings().first()).click();
+    }, () => {
+        let ad = $(".bilibili-player-video-btn-speed-menu-wrap .bilibili-player-active");
+        (ad.prev().length ? ad.prev() : ad.siblings().last()).click();
+    }, () => {
         $(".bilibili-player-iconfont-volume").click();
-    },()=>{
+    }, () => {
         $("[data-text=进入全屏]").click();
-    },()=>{
+    }, () => {
         $(".van-icon-videodetails_like").click();
     }
 ]
-const key=[81,86,66,77,90,191];
-$.each(key,(k,value)=>{
-    kx.core(value,keylist[k]);
+const key = [81, 86, 66, 77, 90, 191];
+$.each(key, (k, value) => {
+    kx.core(value, keylist[k]);
 })
