9b25149058336871a30d5c05cd22944d59c57d2c
zlh <2465898626@qq.com>
2021-07-30 04:19:41 +0800

    4:19

--- kjj.js
+++ kjj.js
@@ -1,5 +1,22 @@
 console.log("1");
 let sss = 0;
+const wblist = [
+    [".bui-switch-input", 81],//弹幕开关
+    ["#图层_1", 90],//全屏
+    [".squirtle-select-item.active:last", 66],//倍速
+    [".squirtle-select-item.active:first", 86],//清晰度
+    [".squirtle-pagefullscreen-inactive", 88],//网页全屏
+    [".squirtle-widescreen-inactive", 32],//网页宽屏
+]
+wblist.forEach((s, i) => {
+    $(document).on("keydown", e => {
+        let u = $(s[1]).next().length ? $(s[1]).next() : $(s[1]).siblings().first()
+        let d = $(s[1]).prev().length ? $(s[1]).prev() : $(s[1]).siblings().last()
+        if (e.ctrlKey && e.keyCode == s[0]) {
+            u.click()
+        }
+    })
+})
 const kx = {
     core(ke, fun) {
         $(document).keydown(event => {
@@ -28,6 +45,6 @@ const keylist = [
     }
 ]
 const key = [81, 86, 66, 77, 90, 191];
-$.each(key, (k, value) => {
-    kx.core(value, keylist[k]);
-})
+// $.each(key, (k, value) => {
+//     kx.core(value, keylist[k]);
+// })
